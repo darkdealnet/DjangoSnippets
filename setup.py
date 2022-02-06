29a092ef513bcd7db6eea31a4d63e0ed2a7d93b6
darkdealnet <real@darkdeal.net>
2022-02-06 22:54:46 +0700

    13

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setuptools.setup(
     python_requires=">=3.10",
     install_requires=[
         'asgiref==3.5.0',
-        'Django==4.0.1,',
+        'Django==4.0.1',
         'django-tinymce==3.4.0',
         'sqlparse==0.4.2',
         'tzdata==2021.5'
