ee74cb36d7e0cb311bc63bb24c7b633900bc39a1
darkdealnet <real@darkdeal.net>
2022-02-07 00:18:36 +0700

    0.0.2

--- src/django_seo_module/models.py
+++ src/django_seo_module/models.py
@@ -5,14 +5,21 @@ from django.utils.html import escape
 class SeoModel(models.Model):
     title = models.CharField(
         max_length=80, help_text='max 80 characters',
-        blank=True
+        blank=True,
+        null=True
     )
     description = models.TextField(
         max_length=160, help_text='max 160 characters',
-        blank=True
+        blank=True,
+        null=True
     )
     keywords = models.TextField(blank=True)
-    slug = models.SlugField(max_length=100, help_text='For URL Page')
+    slug = models.SlugField(
+        max_length=100,
+        help_text='For URL Page',
+        blank=True,
+        null=True
+    )
     index = models.BooleanField(
         default=True,
         help_text=escape(
